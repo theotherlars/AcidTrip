ed1766b278c2cfbf9448119861a2b19e2ea9bf8b
Lars Gøran Hansen <larsern90@gmail.com>
2022-08-24 23:48:42 +0200

    Added happy meal

--- Assets/HealthyThing.cs
+++ Assets/HealthyThing.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HealthyThing : MonoBehaviour
 {
+    private void Update() {
+        transform.eulerAngles = new Vector3 (transform.eulerAngles.x , transform.rotation.y + Time.time * 100f, transform.eulerAngles.z);
+    }
+    
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.CompareTag("Player")){
             if(other.gameObject.TryGetComponent(out PlayerManager playerManager)){
